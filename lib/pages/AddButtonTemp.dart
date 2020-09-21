c47ec85b82137a11a8719d30bcfcbedb23949015
kingk5 <47916231+kingk5@users.noreply.github.com>
2020-09-22 02:35:18 +0530

    front page complete

--- lib/pages/AddButtonTemp.dart
+++ lib/pages/AddButtonTemp.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddButtonTemp extends StatelessWidget {
@@ -8,13 +9,100 @@ class AddButtonTemp extends StatelessWidget {
 //    hasPermissions();
   }
 
+  Future<List<String>> fetch() async {
+    CollectionReference collectionReference =
+        Firestore.instance.collection('food');
+    Map<String, dynamic> m = {};
+    List<String> temp = [];
+    collectionReference.snapshots().listen((snapshot) {
+      List data;
+      data = snapshot.documents;
+      data.forEach((element) {
+        m[element.documentID.toString()] = element.data;
+        Map<String, dynamic> x = element.data;
+        x.forEach((key, value) {
+          temp.add(key.toString());
+        });
+      });
+    });
+    return temp;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.5,
-      height: MediaQuery.of(context).size.height * 0.5,
-      color: Colors.black54,
-      child: Text('//TEMP FILE WILL BE REMOVED LATER//'),
+    return FutureBuilder(
+        future: fetch(),
+        builder: (context, snapshot) {
+          print(snapshot.data.toString());
+          return Scaffold(
+              appBar: AppBar(title: Text("Search app"), actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: DataSearch(snapshot.data));
+              },
+            )
+          ]));
+        });
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  List<String> food_list = [];
+  DataSearch(this.food_list);
+  final recent = [
+    "bhopal",
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          close(context, null);
+        });
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = query.isEmpty
+        ? recent
+        : food_list.where((p) => p.toString().toLowerCase().contains(query.toLowerCase())).toList();
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.location_city),
+        title: RichText(
+            text: TextSpan(
+                text: suggestions[index].substring(0, query.length),
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                children: [
+              TextSpan(
+                  text: suggestions[index].substring(query.length),
+                  style: TextStyle(color: Colors.grey))
+            ])),
+      ),
+      itemCount: suggestions.length,
     );
+    throw UnimplementedError();
   }
 }
